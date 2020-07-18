a0859827f47d4d2c5d871aabd4ea9e077aaa38d6
Your Name <you@example.com>
2020-07-18 13:01:06 +0800

    log info about remote request user

--- src/main/java/com/oneslide/mybatis/resource/AddressController.java
+++ src/main/java/com/oneslide/mybatis/resource/AddressController.java
@@ -1,12 +1,19 @@
 package com.oneslide.mybatis.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oneslide.mybatis.mapper.AddressMapper;
 import com.oneslide.mybatis.mapper.UsersMapper;
 import com.oneslide.mybatis.model.Address;
 import com.oneslide.mybatis.model.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,6 +21,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("address")
 public class AddressController {
+    Logger logger=LoggerFactory.getLogger(AddressController.class);
 
     @Autowired
     AddressMapper mapper;
@@ -23,13 +31,15 @@ public class AddressController {
 
     @GetMapping("/basicSearch")
     public List<Users> searchAddress(){
+
         return mapper2.findAllNames();
     }
 
 
 
     @GetMapping("/healthCheck")
-    public String checkHealth(){
+    public String checkHealth(HttpServletRequest request)  throws JsonProcessingException {
+        logger.info(String.format("request info:{ \"method\":\"%s\",\"path_info\":\"%s\",\"ip\":\"%s\"}",request.getMethod(),request.getRequestURL(),request.getRemoteHost()));
         return "Hello world";
     }
 }
